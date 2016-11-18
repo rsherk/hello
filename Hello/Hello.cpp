1bad0319bc9333c9efcdcd51700978f16661e748
rsherk <russell.sherk@roguewave.com>
2016-11-18 15:54:11 -0500

    No pch for you

--- Hello/Hello.cpp
+++ Hello/Hello.cpp
@@ -1,13 +1,9 @@
 // Hello.cpp : Defines the entry point for the console application.
 //
 
-#ifdef _WIN32
-#include "stdafx.h"
-#endif
-
 #include <stdio.h>
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
 	int i[1];
 	printf("Value is: %d\n", i[0]);
